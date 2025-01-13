ae025ab909e467eb4a9a3d05e94a9710fbb6f004
smakevkin <smakevkin@mail.ru>
2025-01-13 14:33:47 +1000

    fix according to pr

--- main.cpp
+++ main.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
-#include <windows.h>
 #include "complex.hpp"
 #include "quadeq.hpp"
 
+enum Mode{
+    exet = 0,
+    complexcalcul = 1,
+    qeSolver = 2,
+    incorecct = 3,
+};
+
+
 using namespace std;
 int main() {
-    SetConsoleOutputCP(CP_UTF8);
-    string mode = "3";
+    Mode mode = Mode::incorecct;
     cout << "Программа выполняет функции: калькулятор комплексных чисел, решение квадратных уравнений. Чтобы программма завершилась напишите 0\n";
-    while (mode!="0"){
-        mode = "3";
-        while (mode != "1" && mode != "2" && mode != "0") {
+    while (mode!=Mode::exet){
+        mode = Mode::incorecct;
+        while (mode != Mode::complexcalcul && mode != Mode::qeSolver && mode != Mode::exet) {
             cout << "Напиши нужный модуль (1 - арифметика комплексных чисел, 2 - квадратные уравнения): ";
-            cin >> mode;
-            if (mode != "0" && mode != "1" && mode != "2" ) {
+            int temp;
+            cin >> temp;
+            mode = static_cast<Mode>(temp);
+            if (mode != Mode::exet && mode != Mode::complexcalcul && mode != Mode::qeSolver ) {
                 cout << "kys\n";
             }
         }
         cout << endl;
-        switch (stoi(mode))
+        switch (mode)
         {
         case 0:
             break;
